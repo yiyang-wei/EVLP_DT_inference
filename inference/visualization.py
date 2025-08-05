b8564050b4b8758e97e8196aff21653d6645a2f4
yiyang-wei <y238wei@uwaterloo.ca>
2025-08-05 19:00:12 -0400

    updated visualization wide mode

--- inference/visualization.py
+++ inference/visualization.py
@@ -91,7 +91,7 @@ def hourly_all_features_line_plot(hourly_prediction: pd.DataFrame, wide: bool = 
     fig.update_layout(
         title_x=0.5,
         title_xanchor="center",
-        title_y=0.98,
+        title_y=0.98 if wide else 0.99,
         legend_title_text="",
         legend=dict(orientation="h", yanchor="bottom", y=1.05, xanchor="center", x=0.5),
         margin=dict(t=100),
@@ -259,7 +259,7 @@ def protein_line_plot(protein_prediction: pd.DataFrame, wide: bool = True):
     fig.update_layout(
         title_x=0.5,
         title_xanchor="center",
-        title_y=0.98,
+        title_y=0.98 if wide else 0.99,
         legend_title_text="",
         legend=dict(orientation="h", yanchor="bottom", y=1.12, xanchor="center", x=0.5),
         margin=dict(t=100),
